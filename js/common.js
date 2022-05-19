df8e034577c90d67bfa8f66b3c57547f7469a285
jung <phg0675@naver.com>
2022-05-19 17:13:34 +0900

    toss_up

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 
 $(function(){
 $(".s_mini_inner").css({"padding": "0px", "margin": "0px 0px 150px"})
+$(".s_mini_inner img").css({"flex-basis": "calc(50% - 30px)", "max-width": "calc(50% - 30px)"})
 $(".s_mini_txt_inner").css({"flex-basis": "calc(50% - 30px)", "max-width": "calc(50% - 30px)"})
-$(".odd_inner img").css({"flex-basis": "calc(50% - 30px)", "max-width": "calc(50% - 30px)"})
+$(".four_img_box").css({})
 });
